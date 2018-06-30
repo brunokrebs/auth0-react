13b27c423cfd394bf6d7ea5a70dcad473a752a49
Nirmalya Ghosh <nirmalya.email@gmail.com>
2018-06-30 15:10:04 +0530

    feat(authentication): add auth0 authentication

--- src/components/functional/app/index.js
+++ src/components/functional/app/index.js
@@ -1,16 +1,65 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { Layout } from "antd";
+import { Layout, Button, Menu } from "antd";
 
 import Users from "../../../pages/users";
+import Auth from "../../../helpers/auth";
+
+const auth = new Auth();
 
 class App extends React.Component {
+  handleAuthentication = (nextState, replace) => {
+    if (/access_token|id_token|error/.test(nextState.location.hash)) {
+      auth.handleAuthentication();
+    }
+  };
+
+  authenticationButtonNode = () => {
+    return auth.isAuthenticated() ? (
+      <Button
+        onClick={() => {
+          auth.logout();
+        }}
+      >
+        Log Out
+      </Button>
+    ) : (
+      <Button
+        onClick={() => {
+          auth.login();
+        }}
+      >
+        Log In
+      </Button>
+    );
+  };
+
   render() {
     return (
       <Layout>
-        <Layout.Header>Layout.Header</Layout.Header>
+        <Layout.Header
+          style={{
+            background: "#fff",
+            padding: "0",
+            display: "flex",
+            justifyContent: "flex-end"
+          }}
+        >
+          <Menu mode="horizontal" style={{ lineHeight: "64px" }}>
+            <Menu.Item key="1">{this.authenticationButtonNode()}</Menu.Item>
+          </Menu>
+        </Layout.Header>
+
         <Layout.Content>
-          <Route exact path="/" render={() => <Users />} />
+          <Route exact path="/users" render={() => <Users />} />
+          <Route
+            exact
+            path="/callback"
+            render={props => {
+              this.handleAuthentication(props);
+              return <div>Loading...</div>;
+            }}
+          />
         </Layout.Content>
       </Layout>
     );
